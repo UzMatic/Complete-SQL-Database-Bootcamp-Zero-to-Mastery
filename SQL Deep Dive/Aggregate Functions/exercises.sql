9d653599714166565d547bbe46af339f1c0c4280
Uzziah Ngogela <127846165+UzMatic@users.noreply.github.com>
2024-11-15 12:19:42 +0200

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,11 +8,11 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
-
+  The Average salary is 63803.315070224407
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+    1965-02-01
 
 -- BEFORE YOU START
 /*
@@ -23,6 +23,7 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
+    33954
 
 
 -- BEFORE YOU START
@@ -34,6 +35,7 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+    
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
