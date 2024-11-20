94e562a1e6128aeda8b82f9c6f4403313d5047ca
Uzziah Ngogela <127846165+UzMatic@users.noreply.github.com>
2024-11-20 11:23:26 +0200

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -3,9 +3,17 @@
 
 /*
 * Write your query here
-*/
+*/SELECT firstname, lastname, age, income
+FROM customers
+where (age BETWEEN 30 and 50)
+AND (income < 50000);
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
-*/
+*/SELECT AVG(income)
+FROM customers
+WHERE (age BETWEEN 20 AND 50);
+
+The average income between the ages 20 and 50 is = 59361.925908612832
+
