fa2049bb7a92b2a6c3787f12524f1b817b6a80c2
Uzziah Ngogela <127846165+UzMatic@users.noreply.github.com>
2024-11-21 11:53:14 +0200

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,6 +3,9 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
+SELECT first_name, last_name
+FROM employees
+ORDER BY first_name ASC, last_name desc;
 
 
 /*
@@ -10,6 +13,9 @@
 * Table: employees
 * Question: Sort employees by age
 */
+SELECT first_name, last_name, EXTRACT(YEAR FROM AGE(birth_date)) AS Age
+FROM employees
+ORDER BY age;
 
 
 /*
@@ -17,3 +23,7 @@
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+SELECT *
+FROM employees
+WHERE first_name ILIKE 'k%'
+ORDER BY hire_date;
