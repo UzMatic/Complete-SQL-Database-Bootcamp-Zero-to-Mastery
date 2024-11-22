99fd83e9f7c3fa751b38848af661aa9b95754a89
Uzziah Ngogela <127846165+UzMatic@users.noreply.github.com>
2024-11-22 14:45:02 +0200

    Update questions.sql

--- SQL Deep Dive/Having/questions.sql
+++ SQL Deep Dive/Having/questions.sql
@@ -17,6 +17,14 @@ ORDER BY e.emp_no;
 *  Show me all the employees that have had more than 15 salary changes that work in the department development
 *  Database: Employees
 */
+SELECT e.emp_no, COUNT(s.salary) AS "Salary changes"
+FROM employees as e
+INNER JOIN salaries as s USING(emp_no)
+INNER JOIN dept_emp AS d USING(emp_no)
+INNER JOIN departments as de ON de.dept_no = d.dept_no
+WHERE de.dept_name = 'Development'
+GROUP BY e.emp_no
+HAVING COUNT(s.salary) > 15
 
 
 
