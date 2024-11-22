2b8b111d05d06cd48f33c95ee9c316e771f4e556
Uzziah Ngogela <127846165+UzMatic@users.noreply.github.com>
2024-11-22 13:05:14 +0200

    Update questions.sql

--- SQL Deep Dive/Having/questions.sql
+++ SQL Deep Dive/Having/questions.sql
@@ -3,6 +3,13 @@
 *  Show me all the employees, hired after 1991, that have had more than 2 titles
 *  Database: Employees
 */
+SELECT e.emp_no, COUNT(t.title) "Number of titles"
+FROM employees as e
+join titles as t USING(emp_no)
+WHERE EXTRACT(YEAR FROM e.hire_date) > 1991
+GROUP BY e.emp_no
+HAVING COUNT(t.title) > 2
+ORDER BY e.emp_no;
 
 
 
