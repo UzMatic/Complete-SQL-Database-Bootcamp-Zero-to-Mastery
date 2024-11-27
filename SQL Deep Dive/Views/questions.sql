3dd2891670affa49a65483354b45f28dd831a9ce
Uzziah Ngogela <127846165+UzMatic@users.noreply.github.com>
2024-11-27 14:58:34 +0200

    Update questions.sql

--- SQL Deep Dive/Views/questions.sql
+++ SQL Deep Dive/Views/questions.sql
@@ -3,8 +3,11 @@
 *  Shows me all the employees, hired between 1990 and 1995
 *  Database: Employees
 */
-
 CREATE VIEW "90-95" AS
+select *
+FROM employees
+where hire_date between '1990-01-01' and '1995-12-31';
+  
 -- ...
 
 /*
@@ -12,6 +15,11 @@ CREATE VIEW "90-95" AS
 *  Shows me all employees that have ever had a salary over 80000
 *  Database: Employees
 */
-
 CREATE VIEW "bigbucks" AS
--- ...
+SELECT *
+FROM employees e
+JOIN salaries AS s USING (emp_no)
+WHERE s.salary > 80000
+ORDER BY e.emp_no
+
+
