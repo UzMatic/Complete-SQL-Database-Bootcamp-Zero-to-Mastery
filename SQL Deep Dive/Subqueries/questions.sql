891f38d2b3f40f002229fbac4b76e61e2958a2eb
Uzziah Ngogela <127846165+UzMatic@users.noreply.github.com>
2024-11-29 15:56:44 +0200

    Update questions.sql

--- SQL Deep Dive/Subqueries/questions.sql
+++ SQL Deep Dive/Subqueries/questions.sql
@@ -5,6 +5,27 @@
 * Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 * ordered by orderid
 */
+/*with joins*/
+select o.orderid, o.orderdate, o.customerid, c.firstname, c.lastname, c.state
+from orders as o 
+join customers as c using(customerid)
+where state in('OH','NY','OR')
+order by o.customerid;
+/* with subquery*/
+SELECT 
+    o.orderid, 
+    o.orderdate, 
+    o.customerid, 
+    c.firstname, 
+    c.lastname, 
+    c.state
+FROM orders AS o
+JOIN (
+    SELECT customerid, firstname, lastname, state
+    FROM customers
+    WHERE state IN ('OH', 'NY', 'OR')
+) AS c ON o.customerid = c.customerid
+ORDER BY o.customerid;
 
 
 /*
@@ -12,3 +33,8 @@
 * Table: employees
 * Question: Filter employees who have emp_no 110183 as a manager
 */
+/* with join */
+select d.dept_no, d.emp_no , t.title
+from dept_emp as d
+join titles as t using(emp_no)
+WHERE emp_no NOT in ('110183') AND t.title not in ('Manager');
